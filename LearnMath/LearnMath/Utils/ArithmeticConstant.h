06d03b589737b4c924e9b2707ed40f2d5ca4239c
sallyface <2663340751@qq.com>
2025-08-29 19:09:13 +0800

    首页第二次更改

---
+++ LearnMath/LearnMath/Utils/ArithmeticConstant.h
@@ -0,0 +1,13 @@
+//
+//  ArithmeticConstant.h
+//  LearnMath
+//
+//  Created by 基 on 2025/8/29.
+//
+
+#ifndef ArithmeticConstant_h
+#define ArithmeticConstant_h
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define LearnMathScale(x) x*(SCREEN_HEIGHT/812.0)
+
+#endif /* ArithmeticConstant_h */
